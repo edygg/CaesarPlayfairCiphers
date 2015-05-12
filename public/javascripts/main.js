5f95ae646ed079528abbda234babdb1ed2000501
Edilson Gonzalez <edilson.gonza@unitec.edu>
2015-05-12 04:20:45 +0000

    Issue 1: don't compare same string

---
+++ public/javascripts/main.js
@@ -0,0 +1,130 @@
+$(document).ready(function() {
+  // Returns a random integer between min (included) and max (excluded)
+  function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+  
+  function getRandomString(length) {
+    var randomString = "";
+    for (var i = 0; i < length; i++) {
+      randomString += String.fromCharCode(getRandomInt(65, 91));
+    }
+    return randomString;
+  }
+  
+  function caesarCipher(plainText, shift) {
+    console.log(plainText);
+    var cipherString = "";
+    var alphabet = [];
+    
+    for (var i = 0; i < 26; i++) {
+      alphabet[i] = String.fromCharCode(65 + i);
+    }
+    
+    for (var j = 0; j < plainText.length; j++) {
+      var currentCharacterNumber = plainText.charCodeAt(j);
+      cipherString += alphabet[(currentCharacterNumber - 65 + shift) % 26]
+    }
+    
+    return cipherString;
+  }
+  
+  function analizeCipherString(cipherString) {
+    var alphabet = {};
+    for (var i = 0; i < 26; i++) {
+      alphabet[String.fromCharCode(65 + i)] = 0;
+    } 
+    
+    for (var j = 0; j < cipherString.length; j++) {
+      alphabet[cipherString.charAt(j)]++;
+    }
+    
+    return alphabet;
+  }
+  
+  $('#caesar_cipher_form').on('submit', function(e) {
+    e.preventDefault();
+    
+    if ($('#caesar_cipher_shift').val() < 0 || $('#caesar_cipher_shift').val() === '') {
+      alert('Check shift value.')
+      return false;
+    }
+    
+    if ($('#caesar_cipher_string_length').val() <= 0 || $('#caesar_cipher_string_length').val() === '') {
+      alert('Check string length value.')
+      return false;
+    }
+    
+    var cipherString = caesarCipher(getRandomString(parseInt($('#caesar_cipher_string_length').val())), parseInt($('#caesar_cipher_shift').val()));
+    console.log(cipherString);
+    var frequency = analizeCipherString(cipherString);
+    
+    var values = []
+    for (var key in frequency)
+      values.push(frequency[key]);
+    
+    
+    var data = {
+        labels: Object.keys(frequency),
+        datasets: [
+            {
+                label: "Caesar Cipher",
+                fillColor: "rgba(220,220,220,0.5)",
+                strokeColor: "rgba(220,220,220,0.8)",
+                highlightFill: "rgba(220,220,220,0.75)",
+                highlightStroke: "rgba(220,220,220,1)",
+                data: values
+            }
+        ]
+    };
+    
+    
+    var $caesar_context = $('#caesar_chart').get(0).getContext('2d');
+    var $caesar_chart = new Chart($caesar_context).Bar(data);
+    
+    return false;
+  });
+  
+  $('#playfair_cipher_form').on('submit', function(e) {
+    e.preventDefault();
+    if (!(/^[a-z]+$/.test($('#playfair_cipher_key').val()))) {
+      alert("Check key.");
+      return false;
+    }
+    
+    if ($('#playfair_cipher_string_length').val() <= 0 || $('#play_cipher_string_length').val() === '') {
+      alert('Check string length value.');
+      return false;
+    }
+    
+    var cipherString = Playfair(1, getRandomString(parseInt($('#playfair_cipher_string_length').val())), "L", "L", $('#playfair_cipher_key').val(), 0x01);
+    var frequency = analizeCipherString(cipherString);
+    
+    var values = []
+    for (var key in frequency)
+      values.push(frequency[key]);
+    
+    
+    var data = {
+        labels: Object.keys(frequency),
+        datasets: [
+            {
+                label: "Playfair Cipher",
+                fillColor: "rgba(220,220,220,0.5)",
+                strokeColor: "rgba(220,220,220,0.8)",
+                highlightFill: "rgba(220,220,220,0.75)",
+                highlightStroke: "rgba(220,220,220,1)",
+                data: values
+            }
+        ]
+    };
+    
+    var $playfair_context = $('#playfair_chart').get(0).getContext('2d');
+    var $playfair_chart = new Chart($playfair_context).Bar(data);
+    
+    return false;
+  });
+  
+  
+  
+});
